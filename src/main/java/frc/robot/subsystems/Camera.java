d0654ccd9afe6e8f2b1d4d7f2e6b9efeda2db35d
fastassonic <Gabcall04@gmail.com>
2022-02-01 16:36:58 -0600

    Added a camera subsystem and a command for toggling driver mode for use in testing

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.common.hardware.VisionLEDMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Camera extends SubsystemBase {
+  /** Creates a new Camera. */
+  PhotonCamera m_camera;
+  boolean tDrivermode;
+  public Camera() {
+    m_camera = new PhotonCamera("3468");
+    tDrivermode = false;
+    m_camera.setLED(VisionLEDMode.kOn);
+  }
+
+  @Override
+  public void periodic() {
+
+    // This method will be called once per scheduler run
+  }
+  /**  since photon lib uses the var interface when trying to 
+   *  read network tables Please start all methods with "var result = m_camera.getLatestResult();" */
+  public boolean seetarget() { 
+    //Failure to use this in methods will result in a null pointer exception
+    var result = m_camera.getLatestResult(); 
+    return result.hasTargets();
+  }
+  public double latency() { 
+    if(seetarget() == true){
+      var result = m_camera.getLatestResult();
+      return result.getLatencyMillis(); 
+    }
+    else{
+      return -1;
+    }
+  }
+  public void toggledrivermode() {
+    //might not need the flipper but it made me feel cool darn it
+    tDrivermode = !tDrivermode;
+    m_camera.setDriverMode(tDrivermode);
+    if(tDrivermode == true){
+      m_camera.setLED(VisionLEDMode.kOff);
+    }
+    else{m_camera.setLED(VisionLEDMode.kOn);
+    };
+}
+}
