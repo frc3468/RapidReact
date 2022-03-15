0915de0585acd1fe7b8561920088e8daea54e63f
fastassonic <Gabcall04@gmail.com>
2022-03-15 15:35:19 -0500

    Changed autodrive to home before running

--- src/main/java/frc/robot/commands/autonomous/HomeLift.java
+++ src/main/java/frc/robot/commands/autonomous/HomeLift.java
@@ -2,42 +2,39 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.Camera;
+import frc.robot.subsystems.BallLift;
 
-public class tDriverMode extends CommandBase {
-  Camera m_Camera;
-  boolean done;
-  /** Creates a new tDriverMode. */
-  public tDriverMode(Camera camera) {
-    addRequirements(m_Camera); 
-    m_Camera = camera;
-    // Use addRequirements() here to declare subsystem dependencies.
+public class HomeLift extends CommandBase {
+  private BallLift m_subsystem;
+  /** Creates a new RaiseLift. */
+  public HomeLift (BallLift subsystem){
+      m_subsystem = subsystem;
+      addRequirements(m_subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    done = false;
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Camera.toggledrivermode();
-    done = true;
-
+    m_subsystem.lower();
   }
-
+  
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_subsystem.stop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return done;
+    return false;
   }
 }
+
